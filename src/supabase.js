1bdc7c80cc077c6849b04dd6ed07931d707c1f65
Josephine <jrichstad@gmail.com>
2026-03-09 12:35:38 -0600

    connect to supabase

---
+++ src/supabase.js
@@ -0,0 +1,6 @@
+﻿import { createClient } from "@supabase/supabase-js";
+
+const SUPABASE_URL = "https://ckswnigjsktuqwkrcnrp.supabase.co";
+const SUPABASE_KEY = "sb_publishable_syuYd1_YoThDW6_MDInMEA_241tgA5D";
+
+export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
